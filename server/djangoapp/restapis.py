786b8729ec3426a3d6eb163c469a6f240c240872
Foxer456 <lukasfuchs14@gmail.com>
2024-10-21 09:56:50 -0400

    Added until Create Django views to get dealers

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,9 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
+
+
 
 load_dotenv()
 
@@ -11,12 +13,49 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
+def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+    # Prepare URL parameters from kwargs
+    params = ""
+    if kwargs:
+        params = "&".join([f"{key}={value}" for key, value in kwargs.items()])
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+    # Construct the request URL
+    request_url = f"{backend_url}/{endpoint}?{params}"
+    print(f"GET from {request_url}")
 
-# def post_review(data_dict):
-# Add code for posting review
+    try:
+        # Send the GET request
+        response = requests.get(request_url)
+        
+        # Check if the response is successful
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to fetch data: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+        return None
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
+
+def post_review(data_dict):
+    request_url = backend_url + "/insert_review"
+    try:
+        response = requests.post(request_url, json=data_dict)
+        print(response.json())
+        return response.json()
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+        return {"status": "error", "message": "Network exception occurred"}
+
